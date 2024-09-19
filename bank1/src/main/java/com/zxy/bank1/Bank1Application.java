cd4a0fe1d3b765314520fe8a9b9ea26ba53446ef
zhangwei <1102471399@qq.com>
2024-09-19 22:32:11 +0800

    添加eurekak注册服务

--- bank1/src/main/java/com/zxy/bank1/Bank1Application.java
+++ bank1/src/main/java/com/zxy/bank1/Bank1Application.java
@@ -2,8 +2,10 @@ package com.zxy.bank1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Bank1Application {
 
     public static void main(String[] args) {
---
+++ eureka-service/src/test/java/com/zxy/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxy.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
