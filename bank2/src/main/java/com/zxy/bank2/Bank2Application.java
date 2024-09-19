28817f13589fcca39b4f9d5b9482b5c59c18ce7f
zhangwei <1102471399@qq.com>
2024-09-19 22:38:13 +0800

    添加eurekak注册服务

--- bank2/src/main/java/com/zxy/bank2/Bank2Application.java
+++ bank2/src/main/java/com/zxy/bank2/Bank2Application.java
@@ -2,8 +2,10 @@ package com.zxy.bank2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Bank2Application {
 
     public static void main(String[] args) {
